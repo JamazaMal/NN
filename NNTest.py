35645afaa1be298f2f57dda6934a8f036f04c07b
JamazaMal <36673082+JamazaMal@users.noreply.github.com>
2018-09-25 16:04:06 -0600

    From office

--- NNTest.py
+++ NNTest.py
@@ -1,21 +1,13 @@
 import numpy as np
 import NeuralNet
 
-# X = (hours sleeping, hours studying), y = score on test
-X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
-y = np.array(([92], [86], [89]), dtype=float)
+# Inputs
+inputs = np.array(([0, 0], [1, 0], [0, 1], [1, 1]), dtype=float)
+outputs = np.array(([0], [1], [1], [0]), dtype=float)
 
-# scale units
-X = X/np.amax(X, axis=0)  # maximum of X array
-y = y/100  # max test score is 100
+NN = NeuralNet.NeuralNetwork(2, 8, 1)
 
-NN = NeuralNet.NeuralNetwork()
-print("Input: " + str(X))
-print("Output: " + str(y))
+for i in range(1000000):  # trains the NN 1,000 times
+    print("Loss: {}".format(np.mean(np.square(outputs - NN.forward(inputs)))))
+    NN.train(inputs, outputs)
 
-for i in range(1000):  # trains the NN 1,000 times
-    # print ("Input: " + str(X))
-    # print ("Actual Output: " + str(y))
-    # print ("Predicted Output: " + str(NN.forward(X)))
-    print("Loss: " + str(np.mean(np.square(y - NN.forward(X))))) # mean sum squared loss
-    NN.train(X, y)
--- NeuralNet.py
+++ NeuralNet.py
@@ -2,43 +2,49 @@ import numpy as np
 
 
 class NeuralNetwork(object):
-    def __init__(self):
-        # parameters
-        self.inputSize = 2
-        self.outputSize = 1
-        self.hiddenSize = 3
-
-        # weights
-        self.W1 = np.random.randn(self.inputSize, self.hiddenSize) # (3x2) weight matrix from input to hidden layer
-        self.W2 = np.random.randn(self.hiddenSize, self.outputSize) # (3x1) weight matrix from hidden to output layer
-
-    def forward(self, X):
-        # forward propagation through our network
-        self.z = np.dot(X, self.W1) # dot product of X (input) and first set of 3x2 weights
-        self.z2 = self.sigmoid(self.z) # activation function
-        self.z3 = np.dot(self.z2, self.W2) # dot product of hidden layer (z2) and second set of 3x1 weights
-        o = self.sigmoid(self.z3) # final activation function
-        return o
-
-    def sigmoid(self, s):
+    @staticmethod
+    def sigmoid(s):
         # activation function
+        return s/(1+np.abs(s))
         return 1/(1+np.exp(-s))
 
-    def sigmoidPrime(self, s):
+    @staticmethod
+    def sigmoidprime(s):
         # derivative of sigmoid
+        return 1/((np.abs(s)+1)**2)
         return s * (1 - s)
 
-    def backward(self, X, y, o):
-        # backward propgate through the network
-        self.o_error = y - o # error in output
-        self.o_delta = self.o_error*self.sigmoidPrime(o) # applying derivative of sigmoid to error
+    def __init__(self, _i, _h, _o):
+        # class variables
+        self.z2 = None
+        self.o_error = None
+        self.o_delta = None
+        self.z2_error = None
+        self.z2_delta = None
 
-        self.z2_error = self.o_delta.dot(self.W2.T) # z2 error: how much our hidden layer weights contributed to output error
-        self.z2_delta = self.z2_error*self.sigmoidPrime(self.z2) # applying derivative of sigmoid to z2 error
+        self.learningrate = 0.01
 
-        self.W1 += X.T.dot(self.z2_delta) # adjusting first set (input --> hidden) weights
-        self.W2 += self.z2.T.dot(self.o_delta) # adjusting second set (hidden --> output) weights
+        # weights
+        self.W1 = np.random.randn(_i, _h)
+        self.W2 = np.random.randn(_h, _o)
 
-    def train(self, X, y):
-        o = self.forward(X)
-        self.backward(X, y, o)
+    def forward(self, _inputs):
+        # forward propagation through our network
+        self.z2 = self.sigmoid(np.dot(_inputs, self.W1))
+        _outputs = self.sigmoid(np.dot(self.z2, self.W2))
+        return _outputs
+
+    def backward(self, x, y, o):
+        # backward propagate through the network
+        self.o_error = y - o  # error in output
+        self.o_delta = (y - o)*self.sigmoidprime(o)  # applying derivative of sigmoid to error
+
+        self.z2_error = self.o_delta.dot(self.W2.T)  # z2 error: how much our layer weights contributed to output error
+        self.z2_delta = self.z2_error*self.sigmoidprime(self.z2)  # applying derivative of sigmoid to z2 error
+
+        self.W1 += x.T.dot(self.z2_delta)*self.learningrate  # adjusting first set (input --> hidden) weights
+        self.W2 += self.z2.T.dot(self.o_delta)*self.learningrate  # adjusting second set (hidden --> output) weights
+
+    def train(self, x, y):
+        o = self.forward(x)
+        self.backward(x, y, o)
